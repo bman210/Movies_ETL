6822ae2e2b1f35072562bff8714f5eff8c989c8c
bman210 <bman210@hotmail.com>
2022-12-17 19:10:56 -0500

    Add files via upload

---
+++ Movies with Jasmine/table creation.sql
@@ -0,0 +1,9 @@
+create table movies(
+	title text NOT NULL,
+	country text NOT NULL,
+	release_year text NOT NULL,
+	genre text NOT NULL,
+	description text NOT NULL,
+	Streaming_Service text NOT NULL);
+	
+select * from movies
